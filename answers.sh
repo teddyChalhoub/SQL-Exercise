ac6a50021dc9c1428e6172e98daeaf73194d88c3
teddyChalhoub <teddychalhoub0@gmail.com>
2021-06-02 23:32:37 +0300

    Basic Queries

---
+++ answers.sh
@@ -0,0 +1,16 @@
+Basics:
+1. SELECT Name from students; 
+
+2. SELECT * from students WHERE Age > 30;
+
+3. SELECT name from students WHERE Age = 30 AND Gender = "F";
+
+4. SELECT Points from students WHERE name = "Alex";
+
+5. INSERT INTO students (name,Age,Gender,Points) VALUES ("Teddy", 29,"M",100);
+
+6. UPDATE students SET Points = Points + 100 WHERE name = "Basma"; 
+
+7. 
+
+
